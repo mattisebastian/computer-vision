38b94deefe40c0d142c35982c1620de2c9d4fc2a
Matti Leydecker <s3196005@inf.tu-dresden.de>
2014-11-04 12:41:43 +0100

    found all similar colored regions

--- QtCreator/sim_color.cpp
+++ QtCreator/sim_color.cpp
@@ -9,13 +9,16 @@ using namespace cv;
 using namespace std;
 
 int edgeThresh = 1;
-Mat image0, image;
+Mat input, output;
+int DMax = 50;
+
 
 static void help()
 {
-    printf("\nThis sample demonstrates Canny edge detection\n"
-           "Call:\n"
-           "    /.edge [image_name -- Default is fruits.jpg]\n\n");
+    printf("\nThis is an exercise with the goal to find similarly \
+           colored regions\n"
+                            "Call:\n"
+                            "    /.edge [image_name -- Default is fruits.jpg]\n\n");
 }
 
 const char* keys =
@@ -23,23 +26,20 @@ const char* keys =
     "{1| |fruits.jpg|input image name}"
 };
 
-static void onMouse( int event, int x, int y, int, void* )
-{
-    if( event != CV_EVENT_LBUTTONDOWN )
-        return;
-
-    Point seed = Point(x,y);
-    // check if seed is inside the image
-    cout << seed << "\n";
-    cout << x << ", " << y << " pixel is selected.\n";
-
-
-}
-
-
-void simcolor(Mat& myImage, Mat& Result)
+void simcolor(Mat& myImage, Point seed, Mat& Result)
 {
     CV_Assert(myImage.depth() == CV_8U); // accept only uchar image
+
+    // find similar colored regions
+    // set DMax via trackbar
+    int DMax = 50;
+
+    cout << seed << "\n";
+
+    //         << "G: " << intensity.val[1]
+    //         << "R: " << intensity.val[2] << endl;
+
+
 
     Result.create(myImage.size(), myImage.channels());
     //const int nChannels = myImage.channels();
@@ -47,35 +47,92 @@ void simcolor(Mat& myImage, Mat& Result)
     // use filter2D here!
 
 
+}
+
+static void onMouse( int event, int x, int y, int, void* )
+{
+    if( event != CV_EVENT_LBUTTONDOWN )
+        return;
+
+    Point seed = Point(x,y);
+
+    Vec3b selectioncolor = input.at<Vec3b>(seed.y, seed.x);
+    uchar bclick = selectioncolor[0];
+    int bc = static_cast<int>(bclick);
+    uchar gclick = selectioncolor[1];
+    int gc = static_cast<int>(gclick);
+    uchar rclick = selectioncolor[2];
+    int rc = static_cast<int>(rclick);
+
+    vector<Vec3b> simregions;
+
+    // traverse over full input
+    for (int y = 0; y < input.rows; ++y )
+    {
+        // const float* row_ptr = input.ptr<float>(y);
+        for (int x = 0; x < input.cols; ++x)
+        {
+              Vec3b tempcolor = input.at<Vec3b>(y, x);
+
+              // check for color range
+              int bi = static_cast<int>(tempcolor[0]);
+              int gi = static_cast<int>(tempcolor[1]);
+              int ri = static_cast<int>(tempcolor[2]);
+
+
+              float D = sqrt(pow(bc - bi, 2) + pow(gc - gi, 2) + pow(rc - ri, 2));
+
+
+              if(D < DMax)
+              {
+                  simregions.push_back(tempcolor);
+              }
+
+        }
+    }
+
+    cout << "B: " << static_cast<int>(selectioncolor[0]) << endl;
+
+    for( vector<Vec3b>::const_iterator i = simregions.begin(); i != simregions.end(); ++i)
+        cout << *i << ' ';
+
+    // check if seed is inside the input
+    // simcolor(input, seed, resultinput);
 
 }
+
+
+
 
 int main( int argc, char** argv )
 {
     char* filename = argc >= 2 ? argv[1] : (char*)"fruits.jpg";
-    image0 = imread(filename, 1);
+    input = imread(filename, 1);
 
-    if( image0.empty() )
+    if( input.empty() )
     {
-        cout << "Image empty. Usage: ffilldemo <image_name>\n";
+        cout << "input empty. Usage: ffilldemo <input_name>\n";
         return 0;
     }
-    help();
-    image0.copyTo(image);
-    Mat resultimage;
+    if (input.channels() != 3) {
+        cerr << "only works for 3-channel Mat!" << endl;
+    }
 
-    //simcolor(image, resultimage);
+
+    help();
+
+
 
     // Save the image on disk
-    //imwrite("../output_images/Gray_imageNEU.jpg", resultimage);
+    //imwrite("../output_inputs/Gray_inputNEU.jpg", resultimage);
 
     // Show the image
-    namedWindow( "image", CV_WINDOW_AUTOSIZE );
-    //namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
+    namedWindow( "input", CV_WINDOW_AUTOSIZE );
+    //namedWindow( "Gray input", CV_WINDOW_AUTOSIZE );
 
-    setMouseCallback( "image", onMouse, 0 );
+    setMouseCallback( "input", onMouse, 0 );
 
-    imshow( "image", image );
+    imshow( "input", input );
 
 
     // Wait for a key stroke; the same function arranges events processing
