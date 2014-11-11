96efe965d8a54d9fbb06c4b04e8f9d8b9e89c5c7
Matti Leydecker <s3196005@inf.tu-dresden.de>
2014-11-11 13:33:52 +0100

    added source files and a paper describing guided filter

---
+++ QtCreator/task1/task1/fast_guided_filter.hpp
@@ -0,0 +1,17 @@
+#ifndef FAST_GUIDED_FILTER_HPP
+#define FAST_GUIDED_FILTER_HPP
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+
+
+
+#endif // FAST_GUIDED_FILTER_HPP
